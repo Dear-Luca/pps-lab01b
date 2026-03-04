5d2ad488440063325491e3b65312f9bc1d177b9d
Luca Marchi <luca1010.marchi@gmail.com>
2026-03-04 16:44:20 +0100

    test hasKnight hasPawn

--- src/test/java/it/unibo/pps/e2/LogicTest.java
+++ src/test/java/it/unibo/pps/e2/LogicTest.java
@@ -7,38 +7,41 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class LogicTest {
     private Logics logics;
     private static final int SIZE = 5;
+    private static final Pair<Integer, Integer> knight = new Pair<>(0, 0);
+    private static final Pair<Integer, Integer> pawn = new Pair<>(4, 4);
 
     @BeforeEach
-    public void beforEach(){
-        this.logics = new LogicsImpl(SIZE);
+    public void beforeEach() {
+        this.logics = new LogicsImpl(SIZE, knight, pawn);
     }
 
     @Test
-    public void test() {
-        assert (true);
-        // TODO: Add your test logic here
-        // You can generate random inputs and assert the expected output
-        // For example:
-        // int result = Logic.someMethod(5, 10);
-        // assertEquals(expectedResult, result);
+    public void testHasKnight() {
+        assertTrue(logics.hasKnight(knight.getX(), knight.getY()));
     }
 
     @Test
-    public void testInitialBoardIsCorrect() {
+    public void testHasPawn() {
+        assertTrue(logics.hasPawn(pawn.getX(), pawn.getY()));
+    }
+
+    @Test
+    public void testBoardSetUp() {
         boolean hasKnight = false;
         boolean hasPawn = false;
 
         for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++){
-                if (logics.hasKnight(i, j)){
+            for (int j = 0; j < SIZE; j++) {
+                if (logics.hasKnight(i, j)) {
                     hasKnight = true;
                 }
-                if (logics.hasPawn(i, j)){
+                if (logics.hasPawn(i, j)) {
                     hasPawn = true;
                 }
             }
         }
         assertTrue(hasKnight && hasPawn);
     }
+
 
 }
